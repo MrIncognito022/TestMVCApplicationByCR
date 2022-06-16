1cb47c21b0ac678c62891d4a7c4a6ac19c8e93e1
Waqar Kabir <waqarkabir@hotmail.com>
2022-06-16 15:15:12 +0500

    Add project files.

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        { 
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //string s = _config.GetConnectionString("PGHI");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string myName, string myAge, string myCNIC)
+        {
+           var model = new ResultViewModel { Name = myName, Age=myAge, CNIC = myCNIC };
+            return View(model);
+        }
+        public IActionResult Privacy()
+        {
+            
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApp/Models/ResultViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApp.Models
+{
+    public class ResultViewModel
+    {
+        public string Name { get; set; }
+        public string CNIC { get; set; }
+        public string Age { get; set; }
+    }
+}
